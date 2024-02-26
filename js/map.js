bd438845afd39b445b8a451016f7d5c286d05e1b
Paulo Murineddu <701789262a@users.noreply.github.com>
2024-02-26 23:42:03 +0100

    sdfsdf

--- js/map.js
+++ js/map.js
@@ -4,4 +4,22 @@ var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 });
 osm.addTo(map);
-console.log("porcodffffdio");
+console.log("porcodfffdio");
+writeBts();
+
+
+async function writeBts() {
+  console.log("porcoddio");
+    var bts_list = await fetch("http://eolo.zeromist.net/lista_bts.json",
+                    {mode: "cors", method:"GET",headers: { 
+                        "Content-Type": "application/json",
+                        },
+                    }
+            );
+    console.log (`diocai ${bts_list.body}`);
+    bts_json = await bts_list.json();
+    for (bts in bts_json){
+        console.log(`ciaone ${bts}`);
+        //var marker = L.marker([bts['lat'], bts['lng']]).addTo(map);
+    }
+}
