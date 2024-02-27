7e2042a25d870a29e550a1b6ffa96c2a15a00dee
Paulo Murineddu <701789262a@users.noreply.github.com>
2024-02-27 01:23:16 +0100

    sdfsdf

--- js/map.js
+++ js/map.js
@@ -1,3 +1,6 @@
+var parse_georaster = require("georaster");
+var GeoRasterLayer = require("georaster-layer-for-leaflet");
+
 var map = L.map('map').setView([41.9, 12.5], 10);
 var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -6,6 +9,25 @@ var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 osm.addTo(map);
 console.log("porcodfffdioueue");
 writeBts();
+
+var url_to_geotiff_file = "badesi.tif";
+fetch(url_to_geotiff_file)
+  .then(response => response.arrayBuffer())
+  .then(arrayBuffer => {
+    parse_georaster(arrayBuffer).then(georaster => {
+      console.log("georaster:", georaster);
+      var layer = new GeoRasterLayer({
+          georaster: georaster,
+          opacity: 0.7,
+          pixelValuesToColorFn: values => values[0] === 42 ? '#ffffff' : '#000000',
+          resolution: 64 // optional parameter for adjusting display resolution
+      });
+      layer.addTo(map);
+
+      map.fitBounds(layer.getBounds());
+
+  });
+});
 
 
 async function writeBts() {
