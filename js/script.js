d81e1322eda4a9c92dd250c57d719c5ad769ba36
Paulo Murineddu <701789262a@users.noreply.github.com>
2024-02-28 15:41:14 +0100

    werq

--- js/script.js
+++ js/script.js
@@ -2,4 +2,4 @@ let d = new Date();
 tot_bts = 3770;
 
 document.getElementById("sectorcomplete").innerHTML= `121/${tot_bts}`;
-document.getElementById("heatmapcomplete").innerHTML= `1/${tot_bts}`;
+document.getElementById("heatmapcomplete").innerHTML= `2/${tot_bts}`;
