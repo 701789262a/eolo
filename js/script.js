401d5244e5f67c72359df2a8b3fbf953f3fe858a
Paulo Murineddu <701789262a@users.noreply.github.com>
2024-03-01 12:49:22 +0100

    hgf

--- js/script.js
+++ js/script.js
@@ -2,4 +2,4 @@ let d = new Date();
 tot_bts = 3770;
 
 document.getElementById("sectorcomplete").innerHTML= `122/${tot_bts}`;
-document.getElementById("heatmapcomplete").innerHTML= `2/${tot_bts}`;
+document.getElementById("heatmapcomplete").innerHTML= `11/${tot_bts}`;
