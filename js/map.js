b40c548dc1076da2ebca39f2d2b49f2f93b54e33
Paulo Murineddu <701789262a@users.noreply.github.com>
2024-04-01 18:08:16 +0200

    qwerwq

--- js/map.js
+++ js/map.js
@@ -75,9 +75,20 @@ async function writeBts() {
     //map.addLayer(markers);
     makka.push(markers);
 }
+const redIcon = new L.Icon({
+    iconUrl:
+      "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
+    shadowUrl:
+      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
+
 async function queryIvynet(lat,lng,selectedBts){
     marker = L.marker([lat, lng], {
-        
+        icon: redIcon,
         name: selectedBts});
     try{
         layergroup.clearLayers();
