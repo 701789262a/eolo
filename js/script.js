74421bb8fd3945c81d369413e5c60b798c194c5a
Paulo Murineddu <701789262a@users.noreply.github.com>
2024-04-01 22:34:02 +0200

    QWER

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let d = new Date();
 tot_bts = 3770;
 
-document.getElementById("sectorcomplete").innerHTML= `1038/${tot_bts}`;
+document.getElementById("sectorcomplete").innerHTML= `2153/${tot_bts}`;
 document.getElementById("heatmapcomplete").innerHTML= `28/${tot_bts}`;
