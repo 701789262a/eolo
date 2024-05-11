5a4032fed8105b40164e41a4a5f05c93e0711500
Paulo Murineddu <701789262a@users.noreply.github.com>
2024-05-11 17:09:21 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -2,8 +2,4 @@ let d = new Date();
 tot_bts = 3747;
 
 document.getElementById("sectorcomplete").innerHTML= `3747/${tot_bts}`;
-<<<<<<< HEAD
 document.getElementById("heatmapcomplete").innerHTML= `244/${tot_bts}`;
-=======
-document.getElementById("heatmapcomplete").innerHTML= `76/${tot_bts}`;
->>>>>>> 4b7bb7dac358cc5dcd3d18e773891f24a74ae23c
