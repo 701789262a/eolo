3480853bcfce808ca79b2dcae3ed72c2d20f5224
Paulo Murineddu <701789262a@users.noreply.github.com>
2024-05-24 21:14:36 +0200

    sdfsdf

--- js/script.js
+++ js/script.js
@@ -2,4 +2,4 @@ let d = new Date();
 tot_bts = 3747;
 
 document.getElementById("sectorcomplete").innerHTML= ` 3747/${tot_bts}`;
-document.getElementById("heatmapcomplete").innerHTML= ` 821/${tot_bts}`;
+document.getElementById("heatmapcomplete").innerHTML= ` 1002/${tot_bts}`;
