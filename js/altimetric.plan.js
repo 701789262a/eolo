e5b10a548db44e1c3c38b8a883045140caf6a8f2
Paulo Murineddu <701789262a@users.noreply.github.com>
2024-07-01 16:57:28 +0200

    qewr

--- js/altimetric.plan.js
+++ js/altimetric.plan.js
@@ -9,6 +9,9 @@ function generateChart(xValues, yValues) {
         chartStatus.destroy();
     }
     lineofsightz = generateLineOfSight(30,4,xValues, yValues)
+    fresnelPoints = generateFresnelPoints(xValues, yValues)
+    fresnelupline = generateBorderFresnelSight(fresnelPoints,lineofsightz,1)
+    fresneldownline = generateBorderFresnelSight(fresnelPoints,lineofsightz,-1)
 
     //console.log(xValues, yValues, [xValues[0],xValues[xValues.length -1]])
     console.log(lineofsightz)
@@ -73,7 +76,7 @@ function generateChart(xValues, yValues) {
                 data: yValues,
                 borderColor: "darkgray",
                 backgroundColor: "darkgray",
-                pointRadius: 1,
+                pointRadius: 0,
                 cubicInterpolationMode: 'monotone',
                 tension: 0.4,
                 fill: 'start',
@@ -81,6 +84,26 @@ function generateChart(xValues, yValues) {
             },{
                 //labels: [xValues[0],xValues[xValues.length -1]],
                 //data: [yValues[0]+30,yValues[yValues.length -1]+4],
+                //pointBorderWidth:0,
+                pointRadius: 1,
+                data:fresnelupline,
+                borderColor: "rgba(120, 255, 220, 1)",
+
+            },
+            {
+                //labels: [xValues[0],xValues[xValues.length -1]],
+                //data: [yValues[0]+30,yValues[yValues.length -1]+4],
+                //pointBorderWidth:0,
+                pointRadius: 0.1,
+                data:fresneldownline,
+                borderColor: "rgba(120, 255, 220, 1)",
+
+            },
+            {
+                //labels: [xValues[0],xValues[xValues.length -1]],
+                //data: [yValues[0]+30,yValues[yValues.length -1]+4],
+                //pointBorderWidth:0,
+                pointRadius: 1,
                 data:lineofsightz,
                 borderColor: "rgba(6, 120, 220, 1)",
 
@@ -98,4 +121,28 @@ function generateLineOfSight(btsheight, observerheight, xvalues, yvalues){
         lineofsight.push( yvalues[0]+btsheight-(0.1*(xvalues[i]-10)*((yvalues[0]+30-yvalues[yvalues.length -1]-4)/yvalues.length)) )
     }
     return lineofsight
+}
+
+function generateFresnelPoints(xvalues, yvalues){
+    var len = xvalues.length
+    var maxdis = xvalues.slice(-1)[0]
+    var fresnelradius =[]
+    // 0.2 = 1/ghz = 1/5 (prova)
+    for (let i=0; i< len; i+=1){
+        var Fn_i = 17.32*Math.sqrt((0.2*(xvalues[i]/1000)*((maxdis - xvalues[i])/1000))/(maxdis/1000))
+        //console.log(Fn_i)
+        fresnelradius.push(Fn_i)
+    }
+    console.log(fresnelradius)
+    return fresnelradius
+}
+function generateBorderFresnelSight(fresnelpoints,lineofsight,up_down){
+    var newline =[]
+    for (let i = 0;i<lineofsight.length;i+=1){
+        var newpoint = lineofsight[i]+((1*up_down)*fresnelpoints[i])
+        console.log(newpoint)
+        newline.push(newpoint)
+    }
+    console.log(newline)
+    return newline
 }
