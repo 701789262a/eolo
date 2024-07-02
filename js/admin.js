c2a29c99bdc7478ab41e918404bdeca324cac2e7
Paulo Murineddu <701789262a@users.noreply.github.com>
2024-07-02 22:28:48 +0200

    sdfsdf

--- js/admin.js
+++ js/admin.js
@@ -27,21 +27,45 @@ onAuthStateChanged(auth, (user) => {
         getDoc(docRef)
             .then((docSnap) => {
                 console.log(docSnap)
-                if (docSnap.exists()){
+                if (docSnap.exists()) {
                     const userData = docSnap.data();
                     document.getElementById('email').innerHTML = userData.email;
-                }else{
+                } else {
                     console.log('no document found matching id')
                     window.location.replace('https://eolo.zeromist.net/unauthorized.html')
 
                 }
-                
-            }).catch((error)=>{
+
+            }).catch((error) => {
                 console.log("error getting document")
                 window.location.replace('https://eolo.zeromist.net/unauthorized.html')
             })
-    }else{
+    } else {
         console.log('user id not found in local storage')
         window.location.replace('https://eolo.zeromist.net/unauthorized.html')
     }
 })
+
+
+const importManual = document.getElementById('importmanual')
+importManual.onclick= function(e){
+    console.log('PORCODDIO')
+    const id = document.getElementById('id').value
+    const lat = document.getElementById('lat').value
+    const lng = document.getElementById('lng').value
+    const stato = document.getElementById('statoselector').value
+    const data = document.getElementById('data').value
+    const nome = document.getElementById('nome').value
+    const tecno = document.getElementById('tecnoselector').value;
+
+    apiImportBts(id, lat,lng,stato,data, nome, tecno);
+
+}
+
+async function  apiImportBts(id, lat,lng,stato,data, nome, tecno){
+    console.log(`https://eolosector.zeromist.net/importbts?nome=${nome}&id=${id}&lat=${lat}&lng=${lng}&stato=${stato}&data=${data}&tecno=${tecno}`)
+    var response = await fetch(`https://eolosector.zeromist.net/importbts?nome=${nome}&id=${id}&lat=${lat}&lng=${lng}&stato=${stato}&data=${data}&tecno=${tecno}`,{
+        method:'GET'
+    })
+    var res = await response.json()
+}
